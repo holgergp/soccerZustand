235fd3fa404f57441983dee4157b068355b77e58
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-01-19 10:05:38 +0100

    Renaming fails test

--- src/model/Positions.spec.js
+++ src/model/Positions.spec.js
@@ -1,34 +1,33 @@
 import Positions from './Positions';
 
 describe('Positions should', () => {
-  const sampleLeague = [
-    {
-      rank: 1,
-      team: {
-        name: 'Borussia Mönchengladbach',
-        editing: true,
-        id: 'BMG'
-      }
-    },
-    {
-      rank: 2,
-      team: {
-        name: 'Borussia Dortmund',
-        editing: true,
-        id: 'BVB'
-      }
-    },
-    {
-      rank: 3,
-      team: {
-        name: 'FC Bayern München',
-        editing: true,
-        id: 'FCB'
-      }
-    }
-  ];
-
   it('swap two different teams', () => {
+    const sampleLeague = [
+      {
+        rank: 1,
+        team: {
+          name: 'Borussia Mönchengladbach',
+          editing: true,
+          id: 'BMG'
+        }
+      },
+      {
+        rank: 2,
+        team: {
+          name: 'Borussia Dortmund',
+          editing: true,
+          id: 'BVB'
+        }
+      },
+      {
+        rank: 3,
+        team: {
+          name: 'FC Bayern München',
+          editing: true,
+          id: 'FCB'
+        }
+      }
+    ];
     const expectedLeagueState = [
       {
         rank: 1,
@@ -62,5 +61,72 @@ describe('Positions should', () => {
       sampleLeague
     );
     expect(updatedeague).toEqual(expectedLeagueState);
+  });
+  it('rename a specific team in positions', () => {
+    const sampleLeague = [
+      {
+        rank: 1,
+        team: {
+          name: 'Borussia Mönchengladbach',
+          editing: true,
+          id: 'BMG'
+        }
+      },
+      {
+        rank: 2,
+        team: {
+          name: 'Borussia Dortmund',
+          editing: true,
+          id: 'BVB'
+        }
+      },
+      {
+        rank: 3,
+        team: {
+          name: 'FC Bayern München',
+          editing: true,
+          id: 'FCB'
+        }
+      }
+    ];
+    const team = {
+      name: 'Borussia Dortmund',
+      editing: true,
+      id: 'BVB'
+    };
+
+    const leagueWithRenamedLeague = Positions.recalculatePositionsWithRenamedTeam(
+      team,
+      'S04',
+      sampleLeague
+    );
+
+    const expectedLeagueState = [
+      {
+        rank: 1,
+        team: {
+          name: 'Borussia Mönchengladbach',
+          editing: true,
+          id: 'BMG'
+        }
+      },
+      {
+        rank: 2,
+        team: {
+          name: 'FC Bayern München',
+          editing: true,
+          id: 'FCB'
+        }
+      },
+      {
+        rank: 3,
+        team: {
+          name: 'Borussia Dortmund',
+          editing: true,
+          id: 'S04'
+        }
+      }
+    ];
+    expect(leagueWithRenamedLeague).toEqual(expectedLeagueState);
   });
 });
