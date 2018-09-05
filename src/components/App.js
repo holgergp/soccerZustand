8a47d5e81a46851bc3c29841f29f8e229deb7f52
Holger Grosse-Plankermann <holgergp@gmail.com>
2018-09-05 17:50:29 +0200

    applying eslint rules

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './App.css';
-import { Row, Grid } from 'react-bootstrap';
+import {Row, Grid} from 'react-bootstrap';
 import LeagueTable from './LeagueTable';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="index">
-        <Grid>
-          <Row>
-            <LeagueTable />
-          </Row>
-        </Grid>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="index">
+                <Grid>
+                    <Row>
+                        <LeagueTable />
+                    </Row>
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/model/Positions.js
+++ src/model/Positions.js
@@ -1,44 +1,44 @@
 export default class Positions {
   static _findTeamRank = (teamId, positions) => {
-    const zeroBasedIndex = positions.findIndex(team => team.id === teamId);
-    return zeroBasedIndex + 1;
+      const zeroBasedIndex = positions.findIndex(team => team.id === teamId);
+      return zeroBasedIndex + 1;
   };
 
   static _findTeam = (teamId, positions) => {
-    return positions.find(team => team.id === teamId);
+      return positions.find(team => team.id === teamId);
   };
 
   static recalculateSwappedPositions = (
-    sourceTeamId,
-    targetTeamId,
-    currentPositions
+      sourceTeamId,
+      targetTeamId,
+      currentPositions
   ) => {
-    let clonedPositions = currentPositions.slice();
+      const clonedPositions = currentPositions.slice();
 
-    const sourceRank = Positions._findTeamRank(sourceTeamId, clonedPositions);
-    const targetRank = Positions._findTeamRank(targetTeamId, clonedPositions);
+      const sourceRank = Positions._findTeamRank(sourceTeamId, clonedPositions);
+      const targetRank = Positions._findTeamRank(targetTeamId, clonedPositions);
 
-    const sourceTeam = Positions._findTeam(sourceTeamId, clonedPositions);
-    const targetTeam = Positions._findTeam(targetTeamId, clonedPositions);
+      const sourceTeam = Positions._findTeam(sourceTeamId, clonedPositions);
+      const targetTeam = Positions._findTeam(targetTeamId, clonedPositions);
 
-    clonedPositions[targetRank - 1] = sourceTeam;
-    clonedPositions[sourceRank - 1] = targetTeam;
-    return clonedPositions;
+      clonedPositions[targetRank - 1] = sourceTeam;
+      clonedPositions[sourceRank - 1] = targetTeam;
+      return clonedPositions;
   };
 
   static recalculatePositionsWithRenamedTeam = (
-    team,
-    updatedText,
-    currentPositions
+      team,
+      updatedText,
+      currentPositions
   ) => {
-    let clonedPositions = currentPositions.slice();
+      const clonedPositions = currentPositions.slice();
 
-    const teamRank = Positions._findTeamRank(team.id, clonedPositions);
+      const teamRank = Positions._findTeamRank(team.id, clonedPositions);
 
-    //team.editing = false;
-    team.name = updatedText;
+      //team.editing = false;
+      team.name = updatedText;
 
-    clonedPositions[teamRank - 1] = team;
-    return clonedPositions;
+      clonedPositions[teamRank - 1] = team;
+      return clonedPositions;
   };
 }
