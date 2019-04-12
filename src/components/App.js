c71c8cf2ca709ed802be1fd7eb73c09ea1d51a78
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-04-12 09:38:29 +0200

    Removed referenced theme.css (no longer present with bootstrap 4)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import './App.css';
 import {Row, Grid} from 'react-bootstrap';
 import LeagueTable from './LeagueTable';
