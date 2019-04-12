665cb1ba2e86f032d76f5d2a0656163cd8217384
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-04-12 15:01:31 +0200

    Reformat

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import {Row, Container} from 'react-bootstrap';
+import { Row, Container } from 'react-bootstrap';
 import LeagueTable from './LeagueTable';
 
 const App = () => {
-    return (
-        <div className="index">
-            <Container>
-                <Row>
-                    <LeagueTable/>
-                </Row>
-            </Container>
-        </div>);
+  return (
+    <div className="index">
+      <Container>
+        <Row>
+          <LeagueTable />
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Position.js
+++ src/components/Position.js
@@ -1,46 +1,46 @@
 import React from 'react';
-import {DropTarget} from 'react-dnd';
+import { DropTarget } from 'react-dnd';
 import Team from './Team';
 import PropTypes from 'prop-types';
-import {ItemTypes} from '../constants/DndItemTypes';
+import { ItemTypes } from '../constants/DndItemTypes';
 
 const positionTarget = {
-    drop(props) {
-        return props.team;
-    }
+  drop(props) {
+    return props.team;
+  }
 };
 
 const collect = (connect, monitor) => {
-    return {
-        connectDropTarget: connect.dropTarget(),
-        isOver: monitor.isOver(),
-        item: monitor.getItem()
-    };
+  return {
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+    item: monitor.getItem()
+  };
 };
 
-const Position = (props) => {
-    const team = props.team;
-    const rank = props.rank;
-    const updateTeamname = props.updateTeamname;
-    const swapPositions = props.swapPositions;
-    const {connectDropTarget} = props;
-    return connectDropTarget(
-        <div>
-            <Team
-                team={team}
-                rank={rank}
-                updateTeamname={updateTeamname}
-                swapPositions={swapPositions}
-            />
-        </div>
-    );
+const Position = props => {
+  const team = props.team;
+  const rank = props.rank;
+  const updateTeamname = props.updateTeamname;
+  const swapPositions = props.swapPositions;
+  const { connectDropTarget } = props;
+  return connectDropTarget(
+    <div>
+      <Team
+        team={team}
+        rank={rank}
+        updateTeamname={updateTeamname}
+        swapPositions={swapPositions}
+      />
+    </div>
+  );
 };
 
 Position.propTypes = {
-    connectDropTarget: PropTypes.func.isRequired,
-    rank: PropTypes.number.isRequired,
-    swapPositions: PropTypes.func.isRequired,
-    team: PropTypes.object.isRequired,
-    updateTeamname: PropTypes.func.isRequired
+  connectDropTarget: PropTypes.func.isRequired,
+  rank: PropTypes.number.isRequired,
+  swapPositions: PropTypes.func.isRequired,
+  team: PropTypes.object.isRequired,
+  updateTeamname: PropTypes.func.isRequired
 };
 export default DropTarget(ItemTypes.TEAM, positionTarget, collect)(Position);
