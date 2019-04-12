f788c4f785f6079320c7977cd84a18ea9fdab780
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-04-12 11:38:47 +0200

    Run prettier and lint

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -1,32 +1,34 @@
 describe('Webapp visible', () => {
-    beforeEach(() => {
-        cy.visit('/');
-    });
-    it('Change Teamname', () => {
-        const firstTeamSelector = ':nth-child(1)  > .col-md-12 > .textPointer';
-        const firstTeam = cy.get(firstTeamSelector);
-        const newTeamName = 'Der beste Verein';
-        firstTeam.type(`{selectall}{backspace}${newTeamName}`);
-        cy.get(firstTeamSelector).should('contain', newTeamName);
-    });
-    it('Move Team around', () => {
-        const firstTeamSelector = ':nth-child(1) > .col-md-12';
-        const firstTeamNameSelector = ':nth-child(1) > .col-md-12 > .textPointer';
-        const thirdTeamSelector = ':nth-child(3) > .col-md-12';
-        const thirdTeamNameSelector = ':nth-child(3) > .col-md-12 > .textPointer';
-        cy.get(firstTeamNameSelector).invoke('text').as('firstTeamName');
-        cy.get(thirdTeamNameSelector).invoke('text').as('thirdTeamName');
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('Change Teamname', () => {
+    const firstTeamSelector = ':nth-child(1)  > .col-md-12 > .textPointer';
+    const firstTeam = cy.get(firstTeamSelector);
+    const newTeamName = 'Der beste Verein';
+    firstTeam.type(`{selectall}{backspace}${newTeamName}`);
+    cy.get(firstTeamSelector).should('contain', newTeamName);
+  });
+  it('Move Team around', () => {
+    const firstTeamSelector = ':nth-child(1) > .col-md-12';
+    const firstTeamNameSelector = ':nth-child(1) > .col-md-12 > .textPointer';
+    const thirdTeamSelector = ':nth-child(3) > .col-md-12';
+    const thirdTeamNameSelector = ':nth-child(3) > .col-md-12 > .textPointer';
+    cy.get(firstTeamNameSelector)
+      .invoke('text')
+      .as('firstTeamName');
+    cy.get(thirdTeamNameSelector)
+      .invoke('text')
+      .as('thirdTeamName');
 
-        cy.get(firstTeamSelector).drag(thirdTeamSelector);
+    cy.get(firstTeamSelector).drag(thirdTeamSelector);
 
-        cy.get('@firstTeamName').then((firstTeamName) => {
-            cy.get(thirdTeamNameSelector).should('contain', firstTeamName);
-        });
-
-        cy.get('@thirdTeamName').then((thirdTeamName) => {
-            cy.get(firstTeamNameSelector).should('contain', thirdTeamName);
-        });
+    cy.get('@firstTeamName').then(firstTeamName => {
+      cy.get(thirdTeamNameSelector).should('contain', firstTeamName);
     });
+
+    cy.get('@thirdTeamName').then(thirdTeamName => {
+      cy.get(firstTeamNameSelector).should('contain', thirdTeamName);
+    });
+  });
 });
-
-
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,18 +24,21 @@ const dataTransfer = require('./dragSupport');
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-Cypress.Commands.add('drag', {
+Cypress.Commands.add(
+  'drag',
+  {
     prevSubject: 'element'
-}, (sourceSelector, targetSelector) => {
-
+  },
+  (sourceSelector, targetSelector) => {
     cy.wrap(sourceSelector.get(0))
-        .trigger('mousedown', {which: 1})
-        .trigger('dragstart', {dataTransfer})
-        .trigger('drag', {});
+      .trigger('mousedown', { which: 1 })
+      .trigger('dragstart', { dataTransfer })
+      .trigger('drag', {});
 
     cy.get(targetSelector)
-        .trigger('dragover', {dataTransfer})
-        .trigger('drop', {dataTransfer})
-        .trigger('dragend', {dataTransfer})
-        .trigger('mouseup', {which: 1});
-});
+      .trigger('dragover', { dataTransfer })
+      .trigger('drop', { dataTransfer })
+      .trigger('dragend', { dataTransfer })
+      .trigger('mouseup', { which: 1 });
+  }
+);
