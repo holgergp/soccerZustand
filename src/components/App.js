79d53c072dd55a0d3e412cf9404e484f50a3deb0
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-04-12 10:41:36 +0200

    Migrate to bootstrap 4

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-import {Row, Grid} from 'react-bootstrap';
+import {Row, Container} from 'react-bootstrap';
 import LeagueTable from './LeagueTable';
 
 class App extends Component {
     render() {
         return (
             <div className="index">
-                <Grid>
+                <Container>
                     <Row>
                         <LeagueTable />
                     </Row>
-                </Grid>
+                </Container>
             </div>
         );
     }
