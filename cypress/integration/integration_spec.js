080b05e09084589607358e5bf4a68171cceb0bdc
Holger Grosse-Plankermann <holgergp@gmail.com>
2019-04-12 10:57:18 +0200

    Updated test

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -3,17 +3,17 @@ describe('Webapp visible', () => {
         cy.visit('/');
     });
     it('Change Teamname', () => {
-        const firstTeamSelector = ':nth-child(1) > span > .col-md-12 > .textPointer';
+        const firstTeamSelector = ':nth-child(1)  > .col-md-12 > .textPointer';
         const firstTeam = cy.get(firstTeamSelector);
         const newTeamName = 'Der beste Verein';
         firstTeam.type(`{selectall}{backspace}${newTeamName}`);
         cy.get(firstTeamSelector).should('contain', newTeamName);
     });
     it('Move Team around', () => {
-        const firstTeamSelector = ':nth-child(1) > span > .col-md-12';
-        const firstTeamNameSelector = ':nth-child(1) > span > .col-md-12 > .textPointer';
-        const thirdTeamSelector = ':nth-child(3) > span > .col-md-12';
-        const thirdTeamNameSelector = ':nth-child(3) > span > .col-md-12 > .textPointer';
+        const firstTeamSelector = ':nth-child(1) > .col-md-12';
+        const firstTeamNameSelector = ':nth-child(1) > .col-md-12 > .textPointer';
+        const thirdTeamSelector = ':nth-child(3) > .col-md-12';
+        const thirdTeamNameSelector = ':nth-child(3) > .col-md-12 > .textPointer';
         cy.get(firstTeamNameSelector).invoke('text').as('firstTeamName');
         cy.get(thirdTeamNameSelector).invoke('text').as('thirdTeamName');
 
