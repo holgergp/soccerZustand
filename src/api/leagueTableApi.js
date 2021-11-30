670e76b9630c9e2aac83b18e610666af52113482
Holger Grosse-Plankermann <holgergp@gmail.com>
2021-11-30 10:12:40 +0100

    Add editing flag which is not sent from the server

--- src/api/leagueTableApi.js
+++ src/api/leagueTableApi.js
@@ -1,4 +1,4 @@
 export const getSampleData = () =>
-  fetch('https://holgergp.builtwithdark.com/league-table').then((res) =>
-    res.json()
-  );
+  fetch('https://holgergp.builtwithdark.com/league-table')
+    .then((res) => res.json())
+    .then((res) => res.map((t) => ({ ...t, editing: true })));
