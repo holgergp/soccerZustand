31b7bbfe9e39f5d1c3a2963324519addc691d088
Holger Grosse-Plankermann <holgergp@gmail.com>
2021-11-30 15:35:20 +0100

    Explicit clear

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -1,32 +1,33 @@
-describe('Webapp visible', () => {
+describe("Webapp visible", () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.get('.card-header');
+    cy.visit("/");
+    cy.get(".card-header");
   });
-  it('Change Teamname', () => {
-    const firstTeamSelector = ':nth-child(1)  > .col-md-12 > .textPointer';
-    const newTeamName = 'Der beste Verein';
+  it("Change Teamname", () => {
+    const firstTeamSelector = ":nth-child(1)  > .col-md-12 > .textPointer";
+    const newTeamName = "Der beste Verein";
     return cy
       .get(firstTeamSelector)
-      .type(`{selectall}{backspace}${newTeamName}`)
-      .and('contain', newTeamName);
+      .clear()
+      .type(newTeamName)
+      .and("contain", newTeamName);
   });
-  it('Move Team around', () => {
-    const firstTeamSelector = ':nth-child(1) > .col-md-12';
-    const firstTeamNameSelector = ':nth-child(1) > .col-md-12 > .textPointer';
-    const thirdTeamSelector = ':nth-child(3) > .col-md-12';
-    const thirdTeamNameSelector = ':nth-child(3) > .col-md-12 > .textPointer';
-    cy.get(firstTeamNameSelector).invoke('text').as('firstTeamName');
-    cy.get(thirdTeamNameSelector).invoke('text').as('thirdTeamName');
+  it("Move Team around", () => {
+    const firstTeamSelector = ":nth-child(1) > .col-md-12";
+    const firstTeamNameSelector = ":nth-child(1) > .col-md-12 > .textPointer";
+    const thirdTeamSelector = ":nth-child(3) > .col-md-12";
+    const thirdTeamNameSelector = ":nth-child(3) > .col-md-12 > .textPointer";
+    cy.get(firstTeamNameSelector).invoke("text").as("firstTeamName");
+    cy.get(thirdTeamNameSelector).invoke("text").as("thirdTeamName");
 
     cy.get(firstTeamSelector).drag(thirdTeamSelector);
 
-    cy.get('@firstTeamName').then((firstTeamName) => {
-      cy.get(thirdTeamNameSelector).should('contain', firstTeamName);
+    cy.get("@firstTeamName").then((firstTeamName) => {
+      cy.get(thirdTeamNameSelector).should("contain", firstTeamName);
     });
 
-    return cy.get('@thirdTeamName').then((thirdTeamName) => {
-      cy.get(firstTeamNameSelector).should('contain', thirdTeamName);
+    return cy.get("@thirdTeamName").then((thirdTeamName) => {
+      cy.get(firstTeamNameSelector).should("contain", thirdTeamName);
     });
   });
 });
